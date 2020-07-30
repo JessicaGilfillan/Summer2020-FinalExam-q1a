2264f56e1ad929e7594dd79af161a40bebc061c2
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2020-07-29 20:31:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+/* When the button displayed in the browser is clicked, the following should happen:
+
+1.) the background colour of the page changes to  #BADA55
+2.) an H1 is created and added to the page that contains the text ‘Thanks for a fun semester!”
+3.) The button text changes to clicked 
+4.) Good luck on your final exam is removed from the page. 
+
+*/
